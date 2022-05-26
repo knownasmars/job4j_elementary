6ce265f25c3d7dbbe8dbdf3e54cae03ce222c544
Mars <knownasmars@gmail.com>
2022-05-26 12:54:40 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Mars Suleimanov");
+        System.out.println("30.05.1991");
+    }
+}

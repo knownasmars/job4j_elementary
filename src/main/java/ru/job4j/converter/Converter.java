ed3ec2b9127f0832775fb6df1c466f481a20028a
Mars <knownasmars@gmail.com>
2022-05-27 20:23:49 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -22,15 +22,35 @@ public class Converter {
         return rubleFromEuro;
     }
 
+    @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(140);
-        float rubleFromDollar = Converter.dollarToRuble(20);
-        float rubleFromEuro = Converter.euroToRuble(15);
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
 
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("140 rubles are " + dollar + " dollars.");
-        System.out.println("20 dollars are " + rubleFromDollar + " rubles.");
-        System.out.println("15 euros are " + rubleFromEuro + " rubles.");
+        float dollar = Converter.rubleToDollar(120);
+        float in2 = 120;
+        float expected2 = 2;
+        float out2 = Converter.rubleToDollar(120);
+        boolean passed2 = expected2 == out2;
+
+        float rubleFromDollar = Converter.dollarToRuble(20);
+        float in3 = 20;
+        float expected3 = 1200;
+        float out3 = Converter.dollarToRuble(20);
+        boolean passed3 = expected3 == out3;
+
+        float rubleFromEuro = Converter.euroToRuble(15);
+        float in4 = 15;
+        float expected4 = 1050;
+        float out4 = Converter.euroToRuble(15);
+        boolean passed4 = expected4 == out4;
+
+        System.out.println("140 rubles are " + euro + " 2 euros. Test result: " + passed);
+        System.out.println("120 rubles are " + dollar + " 2 dollars. Test result: " + passed2);
+        System.out.println("20 dollars are " + rubleFromDollar + " rubles. Test result: " + passed3);
+        System.out.println("15 euros are " + rubleFromEuro + " rubles. Test result: " + passed4);
     }
 }

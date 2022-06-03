2437ae1bcb2a79c2e39b476e704d7402374cc908
Mars <knownasmars@gmail.com>
2022-06-03 22:14:02 +0300

    5.9. Крест в псевдографике. [#177182 #308304]

--- src/main/java/ru/job4j/loop/Slash.java
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -4,8 +4,8 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = cell == size - 1 - row || cell == row;
-                boolean right = row == size - 1 - cell || row == cell;
+                boolean left = cell == row;
+                boolean right = row == size - 1 - cell;
                 if (left) {
                     System.out.print("X");
                 } else if (right) {

c19384afa7eb8cb9798a86652714846d7d4ffc88
Mars <knownasmars@gmail.com>
2022-06-05 17:27:07 +0300

    6.0.1. Объявление массива. [#3613]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+import java.awt.image.AbstractMultiResolutionImage;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
